8af8a59cd90fc178c37ee770d1b88370429f5e99
DIN-STUDIO <kht1790@gmail.com>
2024-04-18 16:37:07 +0900

    [Refactor] 코드 주석추가 및 코드 리팩토링

--- hyotaek/Week3/BMIApp/BMIApp/ResultViewController.swift
+++ hyotaek/Week3/BMIApp/BMIApp/ResultViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-    @IBOutlet weak var bmiResult: UILabel!
-    @IBOutlet weak var resultMessage: UILabel!
+    @IBOutlet weak var bmiResult: UILabel!      // BMI 숫자 값 label
+    @IBOutlet weak var resultMessage: UILabel!  // 저체중, 정상 등 BMI 결과 메시지 label
     
     var height: Double?
     var weight: Double?
@@ -10,6 +10,7 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MainViewController에서 받은 height, weight 옵셔널 바인딩
         guard let height = height else { return }
         guard let weight = weight else { return }
         
@@ -38,6 +39,8 @@ class ResultViewController: UIViewController {
             self.bmiResult.backgroundColor = UIColor(red: 252/255, green: 232/255, blue: 3/255, alpha: 1)
         }
     }
+    
+    // ResultViewController를 가져와서 있다면 dimiss()로 현재 화면 닫음
     @IBAction func reCalculate(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true)
     }
